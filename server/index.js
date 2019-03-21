8a07fb2e70ca0109e82244620d6ab308d1c83e14
BogdanAngh <bogdananghelache@gmail.com>
2019-03-21 15:06:11 +0200

    added basic user model. added register/login validation.

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
-const express  = require('express');
+const mongoose   = require('mongoose');
+const express    = require('express');
+const bodyParser = require('body-parser');
+const passport   = require('passport');
+
 const keys     = require('./config/keys');
 
 const app = express();
+
+// port for dev or prod 
 const PORT = process.env.PORT || 5000;
 
-
+// establish connection to mongoDB
 mongoose.connect(keys.mongoURI)
     .then(
         () => {console.log('Database is connected')},
         err => {console.log(`Error ${err}`)}
     );
-
-app.get('/', (req, res) => {
-    res.send('Root')
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
